c7f7ea09f1946ed74e5e9ab04dc853382c721417
Leonardo Piñeyro <leopiney@gmail.com>
2019-01-10 22:46:33 -0300

    Fixes unsafe shape definition

--- tensor-safe/src/TensorSafe/Shape.hs
+++ tensor-safe/src/TensorSafe/Shape.hs
@@ -2,15 +2,17 @@
 {-# LANGUAGE GADTs          #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeOperators  #-}
-module TensorSafe.Shape () where
+module TensorSafe.Shape (UnsafeShape(..), Shape(..)) where
 
 import           Data.Proxy        (Proxy (..))
 import           Data.Vector.Sized (Vector, fromList)
 import           GHC.TypeLits      (KnownNat, Nat)
 
-newtype UnsafeShape = UnsafeShape [Int]
-instance Show UnsafeShape
+newtype UnsafeShape = UnsafeShape [Integer] deriving Show
+
 
 data Shape (s :: [Nat]) where
     Nil :: Shape '[]
     (:--) :: KnownNat m => Proxy m -> Shape s -> Shape (m ': s)
+
+infixr 5 :--
