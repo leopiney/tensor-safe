c6cd878b102444e54b8228959f0bbeeaeea44aa7
Leonardo Piñeyro <leopiney@gmail.com>
2019-01-10 23:25:49 -0300

    Added from and to shape functions with examples

--- tensor-safe/src/TensorSafe.hs
+++ tensor-safe/src/TensorSafe.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module TensorSafe
     ( someFunc
     ) where
 
+import           Data.Maybe       (Maybe)
+import           TensorSafe.Shape
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc =
+    let
+        (shape1 :: Maybe (Shape '[4,3])) = fromUnsafe (UnsafeShape [4,3])
+        (shape2 :: Maybe (Shape '[4,3])) = fromUnsafe (UnsafeShape [4,4])
+        (shape3 :: Maybe (Shape '[4,4])) = fromUnsafe (UnsafeShape [4,4]) in
+        do
+            putStrLn $ "Building Shape[4,3] from unsafe list [4,3]: " ++ show shape1
+            putStrLn $ "Building Shape[4,3] from unsafe list [4,4]: " ++ show shape2
+            putStrLn $ "Building Shape[4,4] from unsafe list [4,4]: " ++ show shape3
--- tensor-safe/src/TensorSafe/Shape.hs
+++ tensor-safe/src/TensorSafe/Shape.hs
@@ -1,18 +1,61 @@
-{-# LANGUAGE DataKinds      #-}
-{-# LANGUAGE GADTs          #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE TypeOperators  #-}
-module TensorSafe.Shape (UnsafeShape(..), Shape(..)) where
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE KindSignatures      #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators       #-}
 
-import           Data.Proxy        (Proxy (..))
-import           Data.Vector.Sized (Vector, fromList)
-import           GHC.TypeLits      (KnownNat, Nat)
+module TensorSafe.Shape (
+    Shape(..),
+    UnsafeShape(..),
+    fromUnsafe,
+    toUnsafe,
+) where
 
-newtype UnsafeShape = UnsafeShape [Integer] deriving Show
+import           Data.Proxy   (Proxy (..))
+import           GHC.TypeLits (KnownNat, Nat, natVal)
 
+newtype UnsafeShape = UnsafeShape [Integer] deriving (Eq, Show)
+
+--
+-- Define the safe Shape data kind
+--
+infixr 5 :--
 
 data Shape (s :: [Nat]) where
     Nil :: Shape '[]
     (:--) :: KnownNat m => Proxy m -> Shape s -> Shape (m ': s)
 
-infixr 5 :--
+
+showShape :: (Shape s) -> String
+showShape Nil                       = ""
+showShape ((pm :: Proxy m) :-- Nil) = show (natVal pm)
+showShape ((pm :: Proxy m) :-- s)   = show (natVal pm) ++ "," ++ showShape s
+
+instance Show (Shape s) where
+    show s = "[" ++ (showShape s) ++ "]"
+
+--
+-- Create a MkShape class that allows to convert a regular list
+-- into a safe Shape
+--
+class MkShape (s :: [Nat]) where
+  mkShape :: Shape s
+
+instance MkShape '[] where
+  mkShape = Nil
+
+instance (MkShape s, KnownNat m) => MkShape (m ': s) where
+  mkShape = Proxy :-- mkShape
+
+toUnsafe :: Shape s -> UnsafeShape
+toUnsafe Nil = UnsafeShape []
+toUnsafe ((pm :: Proxy m) :-- s) = UnsafeShape (fromInteger (natVal pm) : s')
+    where
+        (UnsafeShape s') = toUnsafe s
+
+fromUnsafe :: forall s. MkShape s => UnsafeShape -> Maybe (Shape s)
+fromUnsafe shape = if toUnsafe myShape == shape
+    then Just myShape
+    else Nothing
+    where
+        myShape = mkShape :: Shape s
