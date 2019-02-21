48377c9f89945d0095e6251f3928c44c0da6cc5f
Leonardo Piñeyro <leopiney@gmail.com>
2019-02-20 23:36:25 -0300

    Minor changes in tensor examples

--- tensor-safe/src/TensorSafe.hs
+++ tensor-safe/src/TensorSafe.hs
@@ -14,20 +14,20 @@ import           TensorSafe.Tensor
 someFunc :: IO ()
 someFunc =
     let
-        (shape1 :: Shape '[1,3]) = buildShape [1,3]
-        (shape2bad :: Maybe (Shape '[3,1])) = fromUnsafe (UnsafeShape [3,3])
-        (shape2 :: Shape '[3,4]) = fromJust $ fromUnsafe (UnsafeShape [3,4])
-        (shape3 :: Shape '[28,28]) = fromJust $ fromUnsafe (UnsafeShape [28,28])
-        (tensor1 :: Tensor Bool '[28, 28]) = Tensor DT_BOOL shape3
-        (tensor2 :: Tensor Bool '[28, 28]) = Tensor DT_BOOL shape3
-        (tensor3 :: Tensor Bool '[28, 28]) = tensor1 `add` tensor2
-        (tensor4 :: Tensor Float '[1, 3]) = Tensor DT_FLOAT shape1
-        (tensor5 :: Tensor Float '[3, 4]) = Tensor DT_FLOAT shape2
-        -- (tensor6 :: Tensor Float '[1, 6]) = tensor4 `matMult` tensor5 in
+        (shape1 :: Shape [1,3]) = buildShape [1,3]
+        (shape2bad :: Maybe (Shape [3,1])) = fromUnsafe (UnsafeShape [3,3])
+        (shape2 :: Shape [3,4]) = fromJust $ fromUnsafe (UnsafeShape [3,4])
+        (shape3 :: Shape [28,28]) = fromJust $ fromUnsafe (UnsafeShape [28,28])
+        (tensor1 :: Tensor Bool [28, 28]) = Tensor DT_BOOL shape3
+        (tensor2 :: Tensor Bool [28, 28]) = Tensor DT_BOOL shape3
+        (tensor3 :: Tensor Bool [28, 28]) = tensor1 `add` tensor2
+        (tensor4 :: Tensor Float [1, 3]) = Tensor DT_FLOAT shape1
+        (tensor5 :: Tensor Float [3, 4]) = Tensor DT_FLOAT shape2
+        -- (tensor6 :: Tensor Float [1, 6]) = tensor4 `matMult` tensor5 in
         -- Couldn't match type ‘4’ with ‘6’
-        -- Expected type: Tensor Float '[1, 6]
-        --   Actual type: Tensor Float '[1, 4]
-        (tensor6 :: Tensor Float '[1, 4]) = tensor4 `matMult` tensor5 in
+        -- Expected type: Tensor Float [1, 6]
+        --   Actual type: Tensor Float [1, 4]
+        (tensor6 :: Tensor Float [1, 4]) = tensor4 `matMult` tensor5 in
         do
             putStrLn $ "Building Shape[1,3] from unsafe list [1,3]: " ++ show shape1
             putStrLn $ "Building Shape[3,1] from unsafe list [3,3]: " ++ show shape2bad
@@ -49,9 +49,9 @@ someFunc =
 someFunc2 :: IO ()
 someFunc2 =
     let
-        node1_shp :: Shape '[1, 3] = buildShape [1, 3]
+        node1_shp :: Shape [1, 3] = buildShape [1, 3]
         node1                      = constant DT_FLOAT node1_shp
-        node2_shp :: Shape '[1, 3] = buildShape [1, 3]
+        node2_shp :: Shape [1, 3] = buildShape [1, 3]
         node2                      = constant DT_FLOAT node2_shp
         node_add                   = node1 `add` node2 in
     do
