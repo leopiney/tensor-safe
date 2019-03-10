fa611a03c9f1af0ffedbe42e82c0ac0ac56a831d
Leonardo Piñeyro <leopiney@gmail.com>
2019-03-10 04:26:06 -0300

    Added dropout and improved examples outputs

--- tensor-safe/app/Main.hs
+++ tensor-safe/app/Main.hs
@@ -5,6 +5,9 @@ import           TensorSafe
 main :: IO ()
 main = do
     genericTensorExample
+    putStrLn $ "\n\n"
     genericTensorExample2
+    putStrLn $ "\n\n"
     simpleExample
+    putStrLn $ "\n\n"
     mnistExample
