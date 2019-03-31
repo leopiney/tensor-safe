b3e846d4bb5d21331362baaa2d7d6686be4a52d1
Leonardo Piñeyro <leopiney@gmail.com>
2019-03-31 01:21:23 -0300

    Using command line interface built with cmdargs

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,43 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 
-import           System.Environment
+import           System.Console.CmdArgs
 
 import           TensorSafe.Commands.Check    (check)
 import           TensorSafe.Commands.Compile  (compile)
 import           TensorSafe.Commands.Examples (examples)
 
+data TensorSafe = Check   { path :: String }
+                | Compile { path :: String, moduleName :: String }
+                | Examples
+                deriving (Show, Data, Typeable)
+
+cCheck :: TensorSafe
+cCheck = Check { path = def &= help "Path to Haskell module with TensorSafe model inside" }
+
+cCompile :: TensorSafe
+cCompile = Compile {
+    path = def &= help "Path to Haskell module with TensorSafe model inside",
+    moduleName = def &= help "The module name inside the TensorSafe model file" }
+
+cExamples :: TensorSafe
+cExamples = Examples
+
+tensorSafe :: IO TensorSafe
+tensorSafe =
+    cmdArgs (modes [cCheck, cCompile, cExamples])
+    &= verbosity
+    &= help "TensorSafe is a tool to verify Neural Networks models in compilation time"
+    &= summary "TensorSafe v0.0.1, (C) Leonardo Pineyro"
+    &= details [ "TensorSafe can tell if a Neural Network model is well defined and will run without errors",""
+               , "Using a Keras-like API you can define a Neural Network model and TensorSafe will point if there's any error and where"]
+
+
 main :: IO ()
 main = do
-    args <- getArgs
-    let command = head args
-    let commandArgs = tail args
-    case command of
-        "check"    -> check (commandArgs !! 0)
-        "compile"  -> compile (commandArgs !! 0) (commandArgs !! 1)
-        "examples" -> examples
-        d          -> putStrLn $ "Unknown command " ++ d
+    print =<< tensorSafe
+    r <- tensorSafe
+    case r of
+        Check { path = p }                   -> check p
+        Compile { path = p, moduleName = m } -> compile p m
+        Examples                             -> examples
