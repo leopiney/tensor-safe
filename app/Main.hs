471a61abddd84cb7680b033741b0363004c204a6
Leonardo Piñeyro <leopiney@gmail.com>
2019-03-31 01:51:33 -0300

    Minor command line descriptions

--- app/Main.hs
+++ app/Main.hs
@@ -7,37 +7,33 @@ import           TensorSafe.Commands.Check    (check)
 import           TensorSafe.Commands.Compile  (compile)
 import           TensorSafe.Commands.Examples (examples)
 
-data TensorSafe = Check   { path :: String }
-                | Compile { path :: String, moduleName :: String }
+data TensorSafe = Check   { path :: FilePath }
+                | Compile { path :: FilePath, module_name :: String }
                 | Examples
-                deriving (Show, Data, Typeable)
+                deriving (Data, Typeable, Show, Eq)
 
 cCheck :: TensorSafe
-cCheck = Check { path = def &= help "Path to Haskell module with TensorSafe model inside" }
+cCheck = Check
+    { path = def &= typ "PATH" &= help "Path to Haskell module with TensorSafe model inside"
+    } &= help "Checks if a Neural Network model is valid or not"
 
 cCompile :: TensorSafe
-cCompile = Compile {
-    path = def &= help "Path to Haskell module with TensorSafe model inside",
-    moduleName = def &= help "The module name inside the TensorSafe model file" }
+cCompile = Compile
+    { path = def &= typ "PATH" &= help "Path to Haskell module with TensorSafe model inside"
+    , module_name = def &= typ "NAME" &= help "The module name inside the TensorSafe model file (default SomeModule)" &= opt "SomeModule"
+    } &= help "Compiles module and outputs Neural Network model for the specified backend"
 
 cExamples :: TensorSafe
-cExamples = Examples
+cExamples = Examples &= help "Show some examples"
 
 tensorSafe :: IO TensorSafe
-tensorSafe =
-    cmdArgs (modes [cCheck, cCompile, cExamples])
-    &= verbosity
-    &= help "TensorSafe is a tool to verify Neural Networks models in compilation time"
-    &= summary "TensorSafe v0.0.1, (C) Leonardo Pineyro"
-    &= details [ "TensorSafe can tell if a Neural Network model is well defined and will run without errors",""
-               , "Using a Keras-like API you can define a Neural Network model and TensorSafe will point if there's any error and where"]
-
+tensorSafe = cmdArgs (modes [cCompile, cCheck, cExamples])
 
 main :: IO ()
 main = do
     print =<< tensorSafe
     r <- tensorSafe
     case r of
-        Check { path = p }                   -> check p
-        Compile { path = p, moduleName = m } -> compile p m
-        Examples                             -> examples
+        Check { path = p }                    -> check p
+        Compile { path = p, module_name = m } -> compile p m
+        Examples                              -> examples
