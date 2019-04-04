69fb85a1d7376a8bb40108df62959e28babeaa32
Leonardo Piñeyro <leopiney@gmail.com>
2019-04-04 06:17:53 -0300

    Added initial ideas for referencing previous nets
    https://keras.io/getting-started/functional-api-guide/

--- src/TensorSafe/Examples/SimpleLSTMExample.hs
+++ src/TensorSafe/Examples/SimpleLSTMExample.hs
@@ -12,3 +12,42 @@ type MyLSTM = MkINetwork '[LSTM 8 'True] ('D2 10 20) ('D2 10 8)
 
 lstm :: MyLSTM
 lstm = mkINetwork
+
+
+-- type Input = MkINetwork
+--     '[
+--         -- Embedding (output_dim=512, input_dim=10000, input_length=100),
+--         Embedding 512 10000 100,
+--         LSTM 32 'False
+--     ]
+--     ('D1 100)
+--     ('D1 32)
+
+-- type OutputAux = MkINetwork
+--     '[
+--         Reference Aux,
+--         Dense 32 1,
+--         Sigmoid,
+--         Output "aux_output"
+--     ]
+--     ('D1 30)
+--     ('D1 1)
+
+-- type Input2 = MkINetwork '[] ('D1 5) ('D1 5)
+
+-- type Main = MkINetwork
+--     '[
+--         Concatenate (Reference Aux) (Reference Input2)
+--         Dense 37 64,
+--         Relu,
+--         Dense 64 64,
+--         Relu,
+--         Dense 64 64,
+--         Relu,
+--         Dense 64 1,
+--         Sigmoid
+--     ]
+--     ('D1 37)
+--     ('D1 1)
+
+
