d1e0b6d6a6e3de90ac7d3857952c746fe5739bb9
Leonardo Piñeyro <leopiney@gmail.com>
2019-04-04 16:40:50 -0300

    Fixed SomeModule.hs template

--- SomeModule.hs
+++ SomeModule.hs
@@ -15,23 +15,7 @@ import           TensorSafe.Shape
 -- { BEGIN }
 type NN = MkINetwork
     -- { BEGIN NETWORK }
-    '[
-        Conv2D 1 16 3 3 1 1,
-        Relu,
-        MaxPooling 2 2 2 2,
-        Conv2D 16 32 3 3 1 1,
-        Relu,
-        MaxPooling 2 2 2 2,
-        Conv2D 32 32 3 3 1 1,
-        Relu,
-        Flatten,
-        Dense 288 64,
-        Sigmoid,
-        Dense 64 10,
-        Sigmoid
-    ]
-    ('D3 28 28 1)
-    ('D1 10)
+    -- { CONTENT }
     -- { END NETWORK }
 
 nn :: NN
